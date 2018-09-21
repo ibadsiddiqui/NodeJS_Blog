789c48157df03651fcc03191cb473f3da46f98dd
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-09-21 23:03:42 +0500

    path added and home file updated

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const path = require('path')
+
 const express =require('express')
 const app = express();
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
 
 
