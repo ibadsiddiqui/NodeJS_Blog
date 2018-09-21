825ad3a954d81389462ff8d31790f7bb702102b4
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-09-21 23:00:21 +0500

    home page file created and static routing added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express =require('express')
 const app = express();
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    
+})
+
+
 app.listen(4000, ()=> {
     console.log('live at 4000')
 })
