a654130ef4fd1f216ba7a0940b5d698bb48f0a78
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-09-23 00:04:33 +0500

    route added to sample post of post.html file

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.get('/about', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/about.html'))
 })
 
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+
+
 app.listen(4000, ()=> {
     console.log('live at 4000')
 })
