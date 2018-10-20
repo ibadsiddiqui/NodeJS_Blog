b1ecf3b8512ab6bfeefb184d299ba270d9a097dc
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-10-20 15:14:50 +0500

    rendering page for creating new post added

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/posts/new', (req, res)=> {
+    res.render('create')
+})
+
 app.get('/about', (req, res) => {
     res.render('about')
 })
