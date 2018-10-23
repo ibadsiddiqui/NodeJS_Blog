22d3522260ead61e677efa79ab20030484ab9080
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-10-23 21:12:09 +0500

    Added database fields to the Schema

--- database/models/Post.js
+++ database/models/Post.js
@@ -4,6 +4,11 @@ const PostSchma = new mongoose.Schema({
     title: String,
     description: String,
     content: String,
+    username: String,
+    createdAt:{
+        type: Date,
+        default: new Date()
+    }
 })
 const Post = mongoose.model('Post', PostSchma)
 
