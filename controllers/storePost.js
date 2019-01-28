8e026bfbe5cabab73498eaf385588b0955da1e85
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-28 23:57:43 +0500

    console removed for image upload

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -4,10 +4,9 @@ const Post = require('./../database/models/Post')
 module.exports = (req, res) => {
 
     const { image } = req.files;
-    console.log(image);
 
     // you need to create a posts folder in the public directory
-        image.mv(path.resolve(__dirname, '..','public/posts', image.name), error => {
+    image.mv(path.resolve(__dirname, '..', 'public/posts', image.name), error => {
 
         Post.create({
             ...req.body,
