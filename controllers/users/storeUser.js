0647fcc3f56793fed840b3a31efa133e0693d646
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-31 02:21:25 +0500

    handler added for invalid data

--- controllers/users/storeUser.js
+++ controllers/users/storeUser.js
@@ -2,6 +2,8 @@ const User = require('./../../database/models/Users')
 
 module.exports = (req, res) => {
     User.create(req.body, (error, user) => {
+        if (error)
+            return res.render('/auth/register')
         res.redirect('/')
     })
 }
