8bc32dbfb5b753adc8fc25d795245887db3c3c86
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-03 23:21:37 +0500

    file renamed

---
+++ controllers/users/loginUser.js
@@ -0,0 +1,25 @@
+const User = require('./../../database/models/Users')
+const bcrypt = require('bcrypt')
+
+module.exports = (req, res) => {
+    if (req.method === "GET")
+        res.render("login")
+    else {
+        const { email, password } = req.body
+        User.findOne({ email }, (error, user) => {
+            if (error) throw error;
+            if (user) {
+                bcrypt.compare(password, user.password, (error, same) => {
+                    if (error) throw error;
+                    if (same) {
+                        res.redirect('/')
+                    } else {
+                        res.redirect('/auth/login')
+                    }
+                })
+            } else {
+                return res.redirect('/auth/login')
+            }
+        })
+    }
+}
