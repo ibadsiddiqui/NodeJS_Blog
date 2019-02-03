84f9e5cdf75d2a79e65a41b48ddb059d299ba9d7
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-03 23:45:45 +0500

    session saving added on user login

--- controllers/users/loginUser.js
+++ controllers/users/loginUser.js
@@ -12,6 +12,7 @@ module.exports = (req, res) => {
                 bcrypt.compare(password, user.password, (error, same) => {
                     if (error) throw error;
                     if (same) {
+                        req.session.UserId = user._id
                         res.redirect('/')
                     } else {
                         res.redirect('/auth/login')
