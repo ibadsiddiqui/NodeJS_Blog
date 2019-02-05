8284fd0c016c54f40e0156bda5e3707b96255aa5
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-05 15:25:00 +0500

    Diplaying validation errors

--- controllers/users/storeUser.js
+++ controllers/users/storeUser.js
@@ -2,8 +2,11 @@ const User = require('./../../database/models/Users')
 
 module.exports = (req, res) => {
     User.createUser(req.body, (error, user) => {
-        if (error)
+        if (error) {
+            const registrationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+            req.session.registrationErrors = registrationErrors
             return res.render('/auth/register')
+        }
         else
             return res.redirect('/')
     })
