a98aad1aefa2685218e5cd4e393da7ef7e98515e
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-05 15:36:50 +0500

    displaying of errors added

--- controllers/users/createUsers.js
+++ controllers/users/createUsers.js
@@ -1,4 +1,7 @@
 module.exports = (req, res) => {
-    console.log(req.session.registrationErrors)
-    res.render('register')
+    // console.log()
+
+    res.render('register', {
+        errors: req.session.registrationErrors
+    })
 }
