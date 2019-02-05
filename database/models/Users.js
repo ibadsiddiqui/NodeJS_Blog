5bd5befb9b6e5acfce99e867f74c031da5b85ebd
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-05 18:39:19 +0500

    validation on mongoose added

--- database/models/Users.js
+++ database/models/Users.js
@@ -3,16 +3,16 @@ const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: [true, "Please provide your username"]
     },
     email: {
         type: String,
         unique: true,
-        required: true
+        required: [true, "Please provide your email"]
     },
     password: {
         type: String,
-        required: true
+        required: [true, "Please provide your password"]
     }
 });
 
