f63a06f2a4f38d2dc52f8f566e901610dd06aa8b
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-05 18:39:39 +0500

    flash message object registred

--- controllers/users/storeUser.js
+++ controllers/users/storeUser.js
@@ -3,9 +3,12 @@ const User = require('./../../database/models/Users')
 module.exports = (req, res) => {
     User.createUser(req.body, (error, user) => {
         if (error) {
+            console.log(error)
             const registrationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
-            req.session.registrationErrors = registrationErrors
-            return res.render('/auth/register')
+            
+            req.flash('registrationErrors', registrationErrors)
+
+            return res.redirect('/auth/register')
         }
         else
             return res.redirect('/')
